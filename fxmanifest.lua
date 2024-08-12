90e37a32b3f728809b709d5fe0bdae69fb02d4ef
DirkDigglerz <liamsdargie@gmail.com>
2024-08-12 23:04:15 +0100

    yaml fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version 'cerulean'
 lua54 'yes' 
 games { 'rdr3', 'gta5' } 
 author 'DirkScripts' 
-description 'React Boilerplate for FiveM | Uses mantine for theming' 
+description 'Stores | Clean Pack' 
 version      '1.0.0'
 
 shared_script{
