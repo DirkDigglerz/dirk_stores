781f694e19f64dee348ad996c4401a1ca4b5ee01
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 22:16:29 +0000

    chore: bump manifest version to main

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 games { 'rdr3', 'gta5' } 
 author 'DirkScripts' 
 description 'Stores | Clean Pack' 
-version      '1.0.4'
+version      '1.0.5'
 
 shared_script{
   'src/settings/config.lua',
