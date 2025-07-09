0e8f25967b747da4ad22fadde554a228c0eaecc9
DirkDigglerz <liamsdargie@gmail.com>
2025-07-10 00:35:31 +0100

    fix: dirk_lib dependency update

--- src/server/init.lua
+++ src/server/init.lua
@@ -1,5 +1,5 @@
 -- if using qb-core check if qb-shops or qb-pawnshops is running and stop it 
-lib.checkDependency('dirk_lib', '1.2.3', true)
+lib.checkDependency('dirk_lib', '1.2.5', true)
 if lib.settings.framework == 'qb-core' then
   if GetResourceState('qb-shops') == 'started' then
     lib.print.info('Stopping qb-shops as it is not compatible with dirk_stores')
